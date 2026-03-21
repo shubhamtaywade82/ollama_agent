52f5755645151c0b56dfba200934f1e445dd068b
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-03-21 23:05:04 +0530

    Initial Commit

---
+++ lib/ollama_agent.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ollama_agent/version"
+
+module OllamaAgent
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ollama_agent/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OllamaAgent
+  VERSION = "0.1.0"
+end
